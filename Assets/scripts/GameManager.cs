aaae4cf1d9842079f07c2ac0aa6240e0b7e7ff4f
alejandrocy99 <moflycy@gmail.com>
2024-11-04 22:46:06 +0100

    creacion de ui

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
+
+    public event Action OnMainMenu;
+    public event Action OnItemsMenu;
+    public event Action OnARPosition;
+    public static GameManager instance;
+    //nos aseguramos que solo hay una instacia de gamemanager
+    private void Awake(){
+        if(instance!=null && instance !=  this){
+            Destroy(gameObject);
+        }else
+        {
+            instance = this;
+        }
+    }
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainMenu();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void MainMenu(){
+        OnMainMenu?.Invoke();
+        Debug.Log("main menu activated");
+    }
+
+    public void ItemMenu(){
+        OnItemsMenu?.Invoke();
+        Debug.Log("items menu activated");
+    }
+
+    public void ARPosition(){
+        OnARPosition?.Invoke();
+        Debug.Log("AR Position activated");
+    }
+
+    public void CloseApp(){
+        Application.Quit();
     }
 }
---
+++ Assets/scripts/UIManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject mainMenuCanvas;
+    [SerializeField] private GameObject itemsMenuCanvas;
+    [SerializeField] private GameObject ARPositionCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.OnMainMenu += ActivateMainMenu;
+        GameManager.instance.OnItemsMenu += ActivateItemsMenu;
+        GameManager.instance.OnARPosition += ActivateARposition;
+    }
+
+
+
+    private void ActivateMainMenu()
+    {
+        Debug.Log("estoy en active menu");
+        mainMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
+        mainMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
+        mainMenuCanvas.transform.GetChild(2).transform.DOScale(new Vector3(1, 1, 1), 0.3f);
+
+        itemsMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0,0,0), 0.5f);
+        itemsMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0,0,0), 0.3f);
+        itemsMenuCanvas.transform.GetChild(1).transform.DOScaleY(180,0.3f);
+
+        ARPositionCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0,0,0),0.3f);
+        ARPositionCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0,0,0),0.3f);
+
+    }
+
+    private void ActivateItemsMenu()
+    {
+         Debug.Log("estoy en item menu");
+        mainMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0,0,0), 0.3f);
+        mainMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0,0,0), 0.3f);
+        mainMenuCanvas.transform.GetChild(2).transform.DOScale(new Vector3(0,0,0), 0.3f);
+
+        itemsMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1,1,1), 0.5f);
+        itemsMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(1,1,1), 0.3f);
+        itemsMenuCanvas.transform.GetChild(1).transform.DOScaleY(300,0.3f);
+    }
+
+    private void ActivateARposition()
+    {
+         Debug.Log("estoy en ar position");
+        mainMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0,0,0), 0.3f);
+        mainMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0,0,0), 0.3f);
+        mainMenuCanvas.transform.GetChild(2).transform.DOScale(new Vector3(0,0,0), 0.3f);
+
+        itemsMenuCanvas.transform.GetChild(0).transform.DOScale(new Vector3(0,0,0), 0.5f);
+        itemsMenuCanvas.transform.GetChild(1).transform.DOScale(new Vector3(0,0,0), 0.3f);
+        itemsMenuCanvas.transform.GetChild(1).transform.DOScaleY(180,0.3f);
+
+        ARPositionCanvas.transform.GetChild(0).transform.DOScale(new Vector3(1,1,1),0.3f);
+        ARPositionCanvas.transform.GetChild(1).transform.DOScale(new Vector3(1,1,1),0.3f);
+
+    }
+}
